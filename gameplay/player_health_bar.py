030d7ef19d58b75ca35f595fffacf63c870a8732
Angela <angelal4@andrew.cmu.edu>
2025-04-12 11:45:46 -0400

    Update player_health_bar.py with latest changes

---
+++ gameplay/player_health_bar.py
@@ -0,0 +1,5 @@
+from ursina import *
+
+health_bar = Entity(parent=camera.ui, model='quad', color=color.red, scale=(0.5, .05), position=(-.3, .45))
+def updatePlayerHealth(player):
+    healthBar = max(0, player.hp / player.max_hp) * 0.5
