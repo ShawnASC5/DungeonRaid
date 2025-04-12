6df3951e0f499af41f0b84622a22e8a074cf4578
Angela <angelal4@andrew.cmu.edu>
2025-04-12 14:08:51 -0400

    Add player health bar logic

--- gameplay/enemies.py
+++ gameplay/enemies.py
@@ -1,14 +1,29 @@
 from ursina import *
+# from particles import Particles
+# from guns import Bullet
 import random
-
+app = Ursina()
+ 
+#import random position when we know dimensions of the dungeon
 class Enemy(Entity):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        currentEnemy = None
+    def __init__(self, player, move_speed, position = (0,0,0), damage = 10, **kwargs):
+        super().__init__(
+            model = 'quad',
+            texture = "zombieFemaleA.png",
+            position = position,
+            scale = (2,2),
+            **kwargs
+        )        
 
-    def spawnEnemy(self):
-        spawnLocation = random.randint(0,100)
-        self.springEnemy = None
-        self.summerEnemy = None
-        self.autumnEnemy = None
-        self.winterEnemy = None
+player = Entity(model='cube', color=color.orange, position=(0,0,0))
+orc = Enemy(player=player, move_speed=1, position=(3,0,0))
+
+camera.z = -10
+app.run()
+
+# def spawnEnemy(self):
+#     spawnLocation = random.randint(0,100)
+#     self.springEnemy = None
+#     self.summerEnemy = None
+#     self.autumnEnemy = None
+#     self.winterEnemy = None
--- gameplay/player_health_bar.py
+++ gameplay/player_health_bar.py
@@ -2,4 +2,5 @@ from ursina import *
 
 health_bar = Entity(parent=camera.ui, model='quad', color=color.red, scale=(0.5, .05), position=(-.3, .45))
 def updatePlayerHealth(player):
-    healthBar = max(0, player.hp / player.max_hp) * 0.5
+    health_ratio = max(0, player.hp / player.max_hp)
+    health_bar.scale_x = health_ratio * 0.5
